4bbe1734f24de39c3aaa0e24a7e228c4d18253ba
Luiz-Miotto <luiz.l.miotto94@gmail.com>
2022-05-31 18:32:56 -0400

    first git push/ added simple add/sub/mult/div logi

---
+++ script.js
@@ -0,0 +1,19 @@
+function sum(x,y){
+    let z;
+    return z = x+y;
+}
+
+function subtract(x,y){
+    let z;
+    return z = x-y;
+}
+
+function multiply(x,y){
+    let z;
+    return z = x*y;
+}
+
+function divide(x,y){
+    let z;
+    return z = x/y;
+}
