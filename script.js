74da22f2d74976e4c9f97b37f2090238644b268c
Luiz-Miotto <luiz.l.miotto94@gmail.com>
2022-06-01 15:59:29 -0400

    +ed btn & dsply funcs to #'s. clck on btn disps #

--- script.js
+++ script.js
@@ -1,15 +1,78 @@
 let displayValue = "";
 var secondDisplayValue = "";
 let numberArray = [0,1,2,3,4,5,6,7,8,9]
-
+var inputDisplay = document.getElementById("inputDisplay");
 
 var one = document.getElementById("1");
 one.addEventListener("click", ()=>{
     displayValue = displayValue + "1";
     console.log(displayValue);
+    inputDisplay.innerText = displayValue;
 });
 
+var two = document.getElementById("2");
+two.addEventListener("click", ()=>{
+    displayValue = displayValue + "2";
+    console.log(displayValue);
+    inputDisplay.innerText = displayValue;
+});
 
+var three = document.getElementById("3");
+three.addEventListener("click", ()=>{
+    displayValue = displayValue + "3";
+    console.log(displayValue);
+    inputDisplay.innerText = displayValue;
+}); 
+
+var four = document.getElementById("4");
+four.addEventListener("click", () =>{
+    displayValue = displayValue + "4";
+    console.log(displayValue);
+    inputDisplay.innerText = displayValue;
+});
+
+var five = document.getElementById("5");
+five.addEventListener("click", () => {
+    displayValue = displayValue + "5";
+    console.log(displayValue);
+    inputDisplay.innerText = displayValue;
+})
+
+
+var six = document.getElementById("6");
+six.addEventListener("click", () => {
+    displayValue = displayValue + "6";
+    console.log(displayValue);
+    inputDisplay.innerText = displayValue;
+});
+
+var seven = document.getElementById("7");
+seven.addEventListener("click", () => {
+    displayValue = displayValue + "7";
+    console.log(displayValue);
+    inputDisplay.innerText = displayValue;
+});
+
+var eight = document.getElementById("8");
+eight.addEventListener("click", () => {
+    displayValue = displayValue + "8";
+    console.log(displayValue);
+    inputDisplay.innerText = displayValue;
+});
+
+var nine = document.getElementById("9");
+nine.addEventListener("click", () => {
+    displayValue = displayValue + "9";
+    console.log(displayValue);
+    inputDisplay.innerText = displayValue;
+});
+
+var zero = document.getElementById("0");
+zero.addEventListener("click", () => {
+    displayValue = displayValue + "0";
+    console.log(displayValue);
+    inputDisplay.innerText = displayValue;
+});
 
 function sum(x,y){
     let z;
