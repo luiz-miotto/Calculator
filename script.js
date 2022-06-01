06f323eda7de2001969a78b90bd4760b84156fdb
Luiz-Miotto <luiz.l.miotto94@gmail.com>
2022-06-01 12:54:48 -0400

    added buttons and added simple number press funcs

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let displayValue = "";
+var secondDisplayValue = "";
+let numberArray = [0,1,2,3,4,5,6,7,8,9]
+
+
+var one = document.getElementById("1");
+one.addEventListener("click", ()=>{
+    displayValue = displayValue + "1";
+    console.log(displayValue);
+});
+
+
+
 function sum(x,y){
     let z;
     return z = x+y;
@@ -17,3 +30,9 @@ function divide(x,y){
     let z;
     return z = x/y;
 }
+
+
+ var sumFunction = document.getElementById("plus");
+ sumFunction.addEventListener("click", () =>{
+ console.log("testing plus sign")
+ });
